6fde3231d6ad33e018cfa811cefd69f707a9bb44
Anatolii Ivanov <gotnickname@gmail.com>
2023-11-19 09:24:30 +0530

    updated models as per DDD

--- BlazorAppWithMediatrAndDapper.BLL/Models/Order.cs
+++ BlazorAppWithMediatrAndDapper.BLL/Models/Order.cs
@@ -9,6 +9,46 @@ public class Order
 	public DateOnly Date {  get; private set; }
 	public Provider Provider { get; private set; }
 
-	internal List<OrderItem> _items = new();
+	internal List<OrderItem> _items;
 	public ReadOnlyCollection<OrderItem> Items => _items.AsReadOnly();
+
+	private Order() { }
+	public Order(string number, Provider provider, List<OrderItem> items = null)
+	{
+		if (string.IsNullOrEmpty(number))
+		{
+			throw new ArgumentException("OrderItem name cannot be empty", nameof(number));
+		}
+		Number = number;
+		Date = DateOnly.FromDateTime(DateTime.Now);
+		Provider = provider;
+		if (items != null)
+		{
+			_items = items;
+		}
+		else
+		{
+			_items = [];
+		}
+	}
+
+	public void AddItem(OrderItem item)
+	{
+		if (item.OrderId != this.Id)
+		{
+			throw new ArgumentException("OrderItem should have correct OrderId", nameof(item.OrderId));
+		}
+		if (item.Name == this.Number)
+		{
+			// Custom business logic for agregate.
+			throw new ArgumentException("OrderItem's name cannot duplicate order number", nameof(item.Name));
+		}
+		_items.Add(item);
+	}
+
+	public void RemoveItem(int id)
+	{
+		var foundItem = _items.Single(x => x.Id == id);
+		_items.Remove(foundItem);
+	}
 }
--- BlazorAppWithMediatrAndDapper.BLL/Models/OrderItem.cs
+++ BlazorAppWithMediatrAndDapper.BLL/Models/OrderItem.cs
@@ -3,7 +3,35 @@
 public class OrderItem
 {
 	public int Id { get; private set; }
+	public int OrderId { get; private set; }
 	public string Name { get; private set; }
 	public uint Quantity { get; private set; }
 	public string Unit { get; private set; }
+
+	private OrderItem() { }
+	internal OrderItem(int orderId, string name, uint quantity, string unit, int id = default)
+	{
+		if (orderId < 1)
+		{
+			throw new ArgumentException("OrderItem needs to be apllyed to an order", nameof(orderId));
+		}
+		if (string.IsNullOrEmpty(name))
+		{
+			throw new ArgumentException("OrderItem name cannot be empty", nameof(name));
+		}
+		if (quantity == 0)
+		{
+			throw new ArgumentException("Quantity must be at least one", nameof(quantity));
+		}
+		if (string.IsNullOrEmpty(unit))
+		{
+			throw new ArgumentException("OrderItem unit cannot be empty", nameof(name));
+		}
+
+		Id = id;
+		OrderId = orderId;
+		Name = name;
+		Quantity = quantity;
+		Unit = unit;
+	}
 }
