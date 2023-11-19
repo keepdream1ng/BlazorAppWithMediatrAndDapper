575cc9fc03919d82e0351b7860e279ae3c81630c
Anatolii Ivanov <gotnickname@gmail.com>
2023-11-19 16:11:52 +0530

    statrt for frontend

---
+++ BlazorAppWithMediatrAndDapper.PLL/BlazorAppWithMediatrAndDapper.PLL.Client/ViewModels/OrderItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppWithMediatrAndDapper.PLL.Client.ViewModels;
+
+public class OrderItemViewModel
+{
+	public int Id { get; set; }
+	public int OrderId { get; set; }
+
+	[Required]
+	public string Name { get; set; }
+	public uint Quantity { get; set; }
+	public string Unit { get; set; }
+}
---
+++ BlazorAppWithMediatrAndDapper.PLL/BlazorAppWithMediatrAndDapper.PLL.Client/ViewModels/OrderOptionsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using BlazorAppWithMediatrAndDapper.BLL.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppWithMediatrAndDapper.PLL.Client.ViewModels;
+
+public class OrderOptionsViewModel
+{
+	public bool ExistingOrder { get; set; }
+	public int OrderId { get; set; }
+	public int Id { get; set; }
+
+	[Required]
+	public string Number { get; set; }
+
+	[Required]
+	[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+	public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
+	[Required]
+	[Range(1, int.MaxValue, ErrorMessage = "Provider must be picked")]
+	public int ProviderId { get; set; }
+
+	public List<OrderItemViewModel> ItemsList { get; set; } = new();
+	public IQueryable<OrderItemViewModel> Items => ItemsList.AsQueryable();
+
+	[Range(1, int.MaxValue, ErrorMessage = "There should be at least one item in the order")]
+	public int ItemListCount => ItemsList.Count;
+}
